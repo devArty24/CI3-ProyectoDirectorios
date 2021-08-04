58b616a3ce4527de1c01b8ee57e4b8f72bef2d35
devArty24 <ghoul100p@gmail.com>
2021-08-03 22:32:59 -0500

    updated

--- backup_DB/baseNegocios.sql
+++ backup_DB/baseNegocios.sql
@@ -13,7 +13,7 @@ MySQL - 5.5.5-10.1.21-MariaDB : Database - baseNegocios
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-USE `baseNegocios`;
+USE `basenegocios`;
 
 /*Table structure for table `bitacora` */
 
